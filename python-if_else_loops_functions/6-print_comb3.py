2ad21c2a855d3181d63755e657a25146ba592827
ramon <r.antovega@gmail.com>
2024-05-08 18:01:16 -0400

    comb3

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,5 @@
 for i in range(10):
     for j in range(i + 1, 10):
         if i != j:
-            print("{:02d}".format(i * 10 + j), end=", " if i != 8 or j != 9 else "\n")
+            print("{:02d}".format(i * 10 + j),
+                  end=", " if i != 8 or j != 9 else "\n")
