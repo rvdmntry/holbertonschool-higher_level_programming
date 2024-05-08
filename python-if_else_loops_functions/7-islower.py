26c759e4f095336ac7517b6438b2d7bc82a238bd
ramon <r.antovega@gmail.com>
2024-05-08 18:14:57 -0400

    islower

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def islower(c):
-    # Check if the ASCII value of the character is between 97 ('a') and 122 ('z')
+    # Check if the ASCII value of
+    # the character is between 97 ('a') and 122 ('z')
     return ord('a') <= ord(c) <= ord('z')
 
 if __name__ == "__main__":
