20b43e45a779a2a1d5a6a1c16fbacc3db140102f
ramon <r.antovega@gmail.com>
2024-05-08 19:28:42 -0400

    pow

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -3,11 +3,10 @@
 def pow(a, b):
     return a ** b
 
-
-if __name__ == "__pow__":
+if __name__ == "__main__":
     pow = __import__('11-pow').pow
 
-    print(pow(2, 2)) chmod +x 11-pow.py
+    print(pow(2, 2))
     print(pow(98, 2))
     print(pow(98, 0))
     print(pow(100, -2))
