13d7c713d5a8fda0506258b09f26f01ba08761d4
ramon <r.antovega@gmail.com>
2024-05-10 18:37:26 -0400

    argumetns

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import sys
 
+
 def main():
     args = sys.argv[1:]
     num_args = len(args)
-    
+
     if num_args == 0:
         print("0 arguments.")
     elif num_args == 1:
@@ -14,6 +15,7 @@ def main():
 
     for i, arg in enumerate(args, start=1):
         print(f"{i}: {arg}")
+
 
 if __name__ == "__main__":
     main()
