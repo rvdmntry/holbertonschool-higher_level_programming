fea65f09bbbe6acefa9c0666b75ee06e7a33fb39
ramon <r.antovega@gmail.com>
2024-05-10 18:31:16 -0400

    calculation

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-
-def main(a, b):
-    # Calculate all operations
-    operations = [
-        f"{a} + {b} = {add(a, b)}",
-        f"{a} - {b} = {sub(a, b)}",
-        f"{a} * {b} = {mul(a, b)}",
-        f"{a} / {b} = {div(a, b)}" if b != 0 else f"{a} / {b} = Error"  # Handle division by zero
-    ]
-    # Use a single print function
-    print("\n".join(operations))
-
 if __name__ == "__main__":
-    main(10, 5)
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
