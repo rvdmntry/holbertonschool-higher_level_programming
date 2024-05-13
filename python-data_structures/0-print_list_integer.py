623046fe1372e797863746c4fae6ab7c09f0d12f
ramon <r.antovega@gmail.com>
2024-05-13 10:16:07 -0400

    printlistint

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -4,6 +4,7 @@ def print_list_integer(my_list=[]):
     for integer in my_list:
         print("{:d}".format(integer))
 
+
 if __name__ == "__main__":
     # Test code
     my_list = [1, 2, 3, 4, 5]
