67b369346e754fcb90903cd5d906e9dd969d526f
ramon <r.antovega@gmail.com>
2024-05-14 12:44:38 -0400

    onlydffelmnts

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def only_diff_elements(set_1, set_2):
-    # Use set symmetric difference to find elements present in only one of the sets
+    # Use set symmetric difference to find elements
+    # present in only one of the sets
     return set_1 ^ set_2
