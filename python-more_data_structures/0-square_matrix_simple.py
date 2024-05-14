abf93ba8ead65dde21546ae51e742fcb2a93c3ea
ramon <r.antovega@gmail.com>
2024-05-14 12:28:50 -0400

    sqrmtrxsmpl

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-	# Create a new matrix of the same size with squared values
-	new_matrix = [[element ** 2 for element in row] for row in matrix]
-	return new_matrix
+    # Create a new matrix of the same size with squared values
+    new_matrix = [[element ** 2 for element in row] for row in matrix]
+    return new_matrix
