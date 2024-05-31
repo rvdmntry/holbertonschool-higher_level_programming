923294ab4572f8b16b7b5dcb6a4ed7c194e32d0c
ramon <r.antovega@gmail.com>
2024-05-31 10:46:32 -0400

    05

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -4,7 +4,6 @@ from flask_jwt_extended import JWTManager, create_access_token, jwt_required, ge
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-
 app.config['JWT_SECRET_KEY'] = 'super-secret'
 
 auth = HTTPBasicAuth()
