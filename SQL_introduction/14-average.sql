6f22e1243e28c5b9cb7257184c1bafafead0ed32
ramon <r.antovega@gmail.com>
2024-06-17 19:44:12 -0400

    average

---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- Compute the score average of all records in the table second_table
+-- This query calculates the average score of all rows in second_table
+SELECT AVG(score) AS average
+FROM second_table;
