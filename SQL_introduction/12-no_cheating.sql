d54ed56eeb4c1593c06dfe3b41569b63cec418eb
ramon <r.antovega@gmail.com>
2024-06-17 19:37:44 -0400

    no cheat

---
+++ SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+-- Update the score of Bob to 10 in the table second_table
+-- This query updates the score of the row where the name is Bob
+UPDATE second_table
+SET score = 10
+WHERE name = 'Bob';
