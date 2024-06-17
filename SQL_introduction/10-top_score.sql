cbcabbb90b40cdd89cf64e35689503d438b1c4dc
ramon <r.antovega@gmail.com>
2024-06-17 19:33:13 -0400

    top score

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- List all records of the table second_table
+-- This query retrieves all fields from all rows in second_table, ordered by score in descending order
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
