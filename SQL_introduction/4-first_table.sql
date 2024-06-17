1809888435f6cac62d5bd627fe4d4ecbc099f1f5
ramon <r.antovega@gmail.com>
2024-06-17 19:09:00 -0400

    first table

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Create the table first_table if it does not exist
+-- This query creates a table named first_table with columns id and name
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
