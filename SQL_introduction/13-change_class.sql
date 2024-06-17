a0fc6029372fff53e5d4b5d2186dd1e1d30bc2ac
ramon <r.antovega@gmail.com>
2024-06-17 19:40:29 -0400

    change back

---
+++ SQL_introduction/13-change_class.sql
@@ -0,0 +1,4 @@
+-- Remove all records with a score <= 5 in the table second_table
+-- This query deletes rows from second_table where score is less than or equal to 5
+DELETE FROM second_table
+WHERE score <= 5;
