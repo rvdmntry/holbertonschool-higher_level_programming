cd4633e487f4feed7fdff03c3a3a9522eaa8bd96
ramon <r.antovega@gmail.com>
2024-06-17 19:20:24 -0400

    89

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- Count the number of records with id = 89 in the table first_table
+-- This query counts the number of rows with id = 89 in first_table
+SELECT COUNT(*) FROM first_table WHERE id = 89;
