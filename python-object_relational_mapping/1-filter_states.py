abebf2945c29411e13fcc7d66d6dda5ddfdd1ba9
ramon <r.antovega@gmail.com>
2024-06-21 16:52:31 -0400

    filter states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This script lists all states with a name starting with 'N' from the database hbtn_0e_0_usa.
+This script lists all states with a name starting
+with 'N' from the database hbtn_0e_0_usa.
 It takes 3 arguments: mysql username, mysql password, and database name.
 """
 import MySQLdb
