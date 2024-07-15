3464ade95fc8e0c42c3b7c21d4e14f9267bfa148
ramon <r.antovega@gmail.com>
2024-07-15 15:59:38 -0400

    Extending Dynamic Data Display to Include SQLite in Flask

---
+++ python-server_side_rendering/task_04_db.py
@@ -0,0 +1,95 @@
+from flask import Flask, render_template, request
+import json
+import csv
+import sqlite3
+
+app = Flask(__name__)
+
+
+def read_json(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        return data
+    except Exception as e:
+        return []
+
+
+def read_csv(file_path):
+    products = []
+    try:
+        with open(file_path, 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                row['id'] = int(row['id'])
+                row['price'] = float(row['price'])
+                products.append(row)
+        return products
+    except Exception as e:
+        return []
+
+
+def read_sqlite(db_path):
+    products = []
+    try:
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, name, category, price FROM Products")
+        rows = cursor.fetchall()
+        for row in rows:
+            products.append({
+                'id': row[0],
+                'name': row[1],
+                'category': row[2],
+                'price': row[3]
+            })
+        conn.close()
+        return products
+    except Exception as e:
+        return []
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/products')
+def products():
+    source = request.args.get('source')
+    product_id = request.args.get('id')
+
+    if source == 'json':
+        products = read_json('products.json')
+    elif source == 'csv':
+        products = read_csv('products.csv')
+    elif source == 'sql':
+        products = read_sqlite('products.db')
+    else:
+        return render_template('product_display.html', error="Wrong source")
+
+    if product_id:
+        try:
+            product_id = int(product_id)
+            products = [
+                product for product in products if product['id'] == product_id]
+            if not products:
+                return render_template('product_display.html', error="Product not found")
+        except ValueError:
+            return render_template('product_display.html', error="Invalid product ID")
+
+    return render_template('product_display.html', products=products)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
