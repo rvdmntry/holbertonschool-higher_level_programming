1097113e39015699a3acf88c234b2c70bb70dee3
ramon <r.antovega@gmail.com>
2024-07-26 14:40:43 -0400

    Add red class

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,5 @@
+// Add a click event listener to the element with the ID 'red_header'
+document.querySelector('#red_header').addEventListener('click', function() {
+  // Change the text color of the header element to red
+  document.querySelector('header').style.color = '#FF0000';
+});
